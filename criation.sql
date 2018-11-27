3dddbf9ba37ac70c920a23c43c7cb0958ac0bc9a
guizg <guizgraicer@gmail.com>
2018-11-27 14:54:35 -0200

    Add files via upload

---
+++ criation.sql
@@ -0,0 +1,28 @@
+DROP DATABASE if EXISTS labeled_coco;
+CREATE DATABASE labeled_coco;
+use labeled_coco;
+
+DROP TABLE IF EXISTS Images;
+CREATE TABLE Images (
+   id INT NOT NULL AUTO_INCREMENT,
+   path_url VARCHAR (100) NOT NULL,
+   PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS Label;
+CREATE TABLE Label (
+   id INT NOT NULL AUTO_INCREMENT,
+   name VARCHAR (80) NOT NULL,
+   PRIMARY KEY (id)
+);
+
+
+DROP TABLE IF EXISTS Detected;
+CREATE TABLE Detected(
+    id_image INT NOT NULL,
+    id_label INT NOT NULL,
+    score FLOAT NOT NULL,
+    FOREIGN KEY(id_image) REFERENCES Images(id),
+    FOREIGN KEY(id_label) REFERENCES Label(id),
+    PRIMARY KEY (id_image, id_label)
+);
